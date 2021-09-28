03a1557e663a106bbe2c134b766932a61ceb31e8
Martinek <martinmatostano@gmail.com>
2021-09-28 09:57:12 +0200

    add function of equal numbers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-function totalDistance(n1, n2, n3){
-    return (n1/n2)*(n2+n3) 
+function totalEqual(n1, n2, n3){
+    if(n1==n2 && n1==n3)
+    {
+        return 3;
+    }else if(n1==n2 || n1==n3 || n2 == n3)
+    {
+        return 2;
+    }else {return 0;}
 }
-const towerHeight = 100;
-const heightOfStep = 0.2;
-const lengthOfStep = 0.4;
+const n1=3
+const n2=4
+const n3=3
 
-console.log(totalDistance(towerHeight, heightOfStep, lengthOfStep));
+console.log(totalEqual(n1,n2,n3));
