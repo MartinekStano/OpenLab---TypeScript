6d82b7aa18b3e0b1cd4dd708a2e4fb107d9989ce
Martinek <martinmatostano@gmail.com>
2021-09-28 09:50:11 +0200

    add function of calculation of distance

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-const message: string = 'Hello AppsLab!';
-console.log(message);
+function totalDistance(n1, n2, n3){
+    return (n1/n2)*(n2+n3) 
+}
+const towerHeight = 100;
+const heightOfStep = 0.2;
+const lengthOfStep = 0.4;
+
+console.log(totalDistance(towerHeight, heightOfStep, lengthOfStep));
