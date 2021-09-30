035b7e06035e605fbedd1d34fa828b8b5c5d0b48
Martinek <martinmatostano@gmail.com>
2021-09-30 08:10:22 +0200

    add method of sliceOfPie

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-function totalEqual(n1, n2, n3){
-    if(n1==n2 && n1==n3)
-    {
-        return 3;
-    }else if(n1==n2 || n1==n3 || n2 == n3)
-    {
-        return 2;
-    }else {return 0;}
-}
-const n1=3
-const n2=4
-const n3=3
+function sliceOfPie(totalNumberOfSlices, numberOfRecipients, numberOfSlicesEachPersonGets){
+    var helper = numberOfRecipients * numberOfSlicesEachPersonGets;
 
-console.log(totalEqual(n1,n2,n3));
+    if(helper <= totalNumberOfSlices)
+        {
+            return true;
+        }else{
+            return false;
+        }
+}
+var totalNumberOfSlices = 11;
+var numberOfRecipients = 5;
+var numberOfSlicesEachPersonGets = 2;
+
+var totalNumberOfSlices2 = 11;
+var numberOfRecipients2 = 5;
+var numberOfSlicesEachPersonGets2 = 3;
+
+console.log(sliceOfPie(totalNumberOfSlices, numberOfRecipients, numberOfSlicesEachPersonGets));
+console.log(sliceOfPie(totalNumberOfSlices2, numberOfRecipients2, numberOfSlicesEachPersonGets2));
