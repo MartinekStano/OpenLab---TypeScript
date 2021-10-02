79faad966186ef5916141634b6e95ee3f8680ee7
Martinek <martinmatostano@gmail.com>
2021-10-02 20:15:15 +0200

    add method of warOfNumbers

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
-let word1 = "abc"
-let word2 = "c"
-
-function StringEndWith(word1:string, word2:string)
+var array1:number[] = [2,8,7,5];
+var array2:number[] = [12,90,75];
+var odd:number;
+var even:number;
+function WarOfNumbers(array:number[])
 {
-    if(word1.endsWith(word2))
+    odd = 0;
+    even = 0;
+    for(var i:number =0; i< array.length; i++)
     {
-        return true;
-    }else{
-        return false;
+        if(array[i]%2 == 0)
+        {
+            even +=array[i];
+        }else{
+            odd += array[i]
+        }
+        
     }
+    if(odd<even)
+        {
+            var helper:number = even - odd;
+            console.log("Even Wins with difference of "+helper);
+        }
+        else{
+            var helper:number= odd-even;
+            console.log("Odd Wins with difference of "+helper);
+        }
 }
-
-console.log("Result is: "+StringEndWith(word1, word2))
+WarOfNumbers(array1);
+WarOfNumbers(array2);
