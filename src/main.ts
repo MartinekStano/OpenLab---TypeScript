5b17a8b748116645125b30f0ece99fa8bdb3cabc
Martinek <martinmatostano@gmail.com>
2021-10-02 20:44:53 +0200

    add method of min and max

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,15 @@
-var array1:number[] = [2,8,7,5];
-var array2:number[] = [12,90,75];
-var odd:number;
-var even:number;
-function WarOfNumbers(array:number[])
+var array1:number[] = [10,4,1,4,-10,-50,32,21];
+var array2:number[] = [44,32,86,19];
+function MaxMin(array:number[])
 {
-    odd = 0;
-    even = 0;
-    for(var i:number =0; i< array.length; i++)
-    {
-        if(array[i]%2 == 0)
-        {
-            even +=array[i];
-        }else{
-            odd += array[i]
-        }
-        
-    }
-    if(odd<even)
-        {
-            var helper:number = even - odd;
-            console.log("Even Wins with difference of "+helper);
-        }
-        else{
-            var helper:number= odd-even;
-            console.log("Odd Wins with difference of "+helper);
-        }
+    var helper:number = 0;
+    var max:number = Math.max(...array);
+    var min:number = Math.min(...array);
+    helper = max - min;
+    console.log("The Biggest number is: "+max);
+    console.log("The Smallest number is: "+min);
+    console.log("Result is: "+helper);
+    
 }
-WarOfNumbers(array1);
-WarOfNumbers(array2);
+MaxMin(array1);
+MaxMin(array2);
